257aa9c16e14912f5ad8f11fc43f61efb9e6c8e2
Evgenii Fedorov <evgenii.fedorov@senodis.io>
2022-12-01 11:09:12 +0100

    implemented course 2 week2 assignment

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
+
 
 def detail(request, question_id):
-    return HttpResponse(f'You are looking at question {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -9,6 +14,13 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-    
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index. 576fe2a7!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
+def owner(request):
+    return HttpResponse("Hello, world. bd47dc58 is the polls index.")
